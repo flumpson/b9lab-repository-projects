fbe732263d7185ad6950a2a6151ca0c5c1f9e9eb
Ryan Brandt <ryanbrandt@Ryans-MacBook-Pro-2.local>
2017-12-10 22:23:59 -0500

    adds revisions based on b9 comments

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -4,33 +4,62 @@ pragma solidity ^0.4.4;
 
 
 contract Splitter {
-	address bob;
-	address alice;
-	address owner;
+
+	address public bob;
+	address public alice;
+	address public carol;
+	address public owner;
+	uint public bobsFunds;
+	uint public carolsFunds;
+	bool private paused;
 
 
-	function Splitter(address _bob, address _alice) {
+	function Splitter(address _bob, address _alice, address _carol) {
+		require(_bob != 0 && _alice != 0 && _carol != 0);
 		bob = _bob;
 		alice = _alice;
+		carol = _carol;
 		owner = msg.sender;
+		paused = false;
 	}
 	
-	function splitEther() 
+	function receive() 
 	public
 	payable 
 	returns(bool success){
-	    if(msg.value == 0) throw;
-	    uint amount = this.balance;
-	    bob.send(amount/2);
-	    alice.send(amount/2);
+	    require(msg.value > 0 && paused == false);
+	    if( msg.sender != alice){
+	    	msg.sender.transfer(msg.value);
+	    	return false;
+	    }
+	    bobsFunds = msg.value/2;
+	    carolsFunds = msg.value/2;
+	    return true;
+
+	}
+
+	function withdraw() 
+	public 
+	returns(bool success){
+		require(paused == false);
+		if(msg.sender == bob && bobsFunds > 0){
+			bob.transfer(bobsFunds);
+			bobsFunds = 0;
+		}
+		if(msg.sender == carol && carolsFunds > 0){
+			carol.transfer(carolsFunds);
+			carolsFunds = 0;
+		}
 	    return true;
 	}
 
-	function kill() 
+	function pause()
 	public
-	{
-	    if(msg.sender != owner) throw;
-	    selfdestruct(owner);
+	returns(bool success){
+		require(msg.sender == owner);
+		if(paused == true){ paused = false;}
+		else{paused = true;}
+		return true;
 	}
 	
 }
